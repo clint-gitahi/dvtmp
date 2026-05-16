35fdc18df2ed8877c765abb6a854d29107eaae89
Clinton Gitahi <clingitahi@gmail.com>
2026-05-16 12:37:12 +0300

    feat: initial folder setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@': './src',
+          '@app': './src/app',
+          '@features': './src/features',
+          '@shared': './src/shared',
+          '@types': './src/types',
+        },
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ],
 };
